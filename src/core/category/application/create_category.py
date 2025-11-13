2c6b186adc1f9c9f6bf2908a734788b480515a96
flvSantos15 <flvsantos300@gmail.com>
2025-11-13 18:05:19 -0300

    chore(test_create_category): add test for creating category with invalid data

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -2,6 +2,15 @@ from uuid import UUID
 
 from src.core.category.domain.category import Category
 
-def create_category(name: str, description: str, is_active: bool = True) -> UUID:
-  category = Category(name=name, description=description, is_active=is_active)
-  return category.id
+
+class InvalidCategoryData(Exception):
+    pass
+
+
+def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+    try:
+        category = Category(name=name, description=description, is_active=is_active)
+    except ValueError as err:
+        raise InvalidCategoryData(err)
+
+    return category.id
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,14 +1,27 @@
 import uuid
 
-from src.core.category.application import create_category
+import pytest
+
+# from src.core.category.application import create_category
+from src.core.category.application.create_category import (
+    InvalidCategoryData,
+    create_category,
+)
+
 
 class TestCreateCategory:
-  def test_create_category_with_valid_data(self):
-    category_id = create_category(
-      name="Filme",
-      description="Categoria para filme",
-      is_active=True
-    )
+    def test_create_category_with_valid_data(self):
+        category_id = create_category(
+            name="Filme", description="Categoria para filme", is_active=True
+        )
 
-    assert category_id is not None
-    assert isinstance(category_id, uuid.UUID)
+        assert category_id is not None
+        assert isinstance(category_id, uuid.UUID)
+
+    def test_create_category_with_invalid_data(self):
+        with pytest.raises(InvalidCategoryData, match="name cannot be empty"):
+            category_id = create_category(
+                name="",
+            )
+
+            assert category_id
