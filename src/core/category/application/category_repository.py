2a192b8ac5aa8ea4ef39b821ca68ff137a63d524
flvSantos15 <flvsantos300@gmail.com>
2026-01-08 20:29:36 -0300

    chore: add unit test for delete category

--- src/core/category/application/category_repository.py
+++ src/core/category/application/category_repository.py
@@ -3,12 +3,16 @@ from uuid import UUID
 
 from src.core.category.domain.category import Category
 
-class CategoryRepository(ABC):
-  @abstractmethod
-  def save(self, category):
-    raise NotImplementedError()
-  
-  @abstractmethod
-  def get_by_id(self, id: UUID) -> Category | None:
-    raise NotImplementedError()
 
+class CategoryRepository(ABC):
+    @abstractmethod
+    def save(self, category):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, id: UUID) -> Category | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, id: UUID) -> None:
+        raise NotImplementedError
