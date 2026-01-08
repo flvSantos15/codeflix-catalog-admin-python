0a5ce619f4604f6c5b054cc0bab2b38bf4b90631
flvSantos15 <flvsantos300@gmail.com>
2026-01-08 20:40:18 -0300

    chore: update test in memory test file

--- src/core/category/tests/infra/test_in_memory_category.py
+++ src/core/category/tests/infra/test_in_memory_category.py
@@ -3,14 +3,18 @@ from src.core.category.infra.in_memory_category import InMemoryCategoryRepositor
 
 
 class TestSave:
-  def test_can_save_category(self):
-    repository = InMemoryCategoryRepository()
-    category = Category(name='Clothes', description='Clothes description')
-    repository.save(category)
+    def test_can_save_category(self):
+        repository = InMemoryCategoryRepository()
+        category = Category(name='Clothes', description='Clothes description')
+        repository.save(category)
 
-    assert len(repository.categories) == 1
-    assert repository.categories[0] == category
+        assert len(repository.categories) == 1
+        assert repository.categories[0] == category
 
 
 class TesteGetById:
-  ...
+    ...
+
+
+class TestDelete:
+    ...
