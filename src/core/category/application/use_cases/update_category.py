fdbff5b6de4916f2a6c5048b6a7d7f778f4b7b8b
flvSantos15 <flvsantos300@gmail.com>
2026-01-09 12:50:31 -0300

    chore: add unti category test for updating name and description

---
+++ src/core/category/application/use_cases/update_category.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+from uuid import UUID
+from core.category.application.category_repository import CategoryRepository
+
+
+@abstractmethod
+class UpdateCategoryRequest:
+    id: UUID
+    name: str | None = None
+    description: str | None = None
+
+
+class UpdateCategory:
+    def __init__(self, repository: CategoryRepository):
+        self.repository = repository
+
+    def execute(self, request: UpdateCategoryRequest) -> None:
+        category = self.respository.get_by_id(request)
+
+        current_name = category.name
+        current_description = category.description
+
+        if request.name is not None:
+            current_name = request.name
+
+        if request.description is not None:
+            current_description = request.description
+
+        category.update_category(
+            name=current_name,
+            description=current_description
+        )
+
+        self.repository.update(category)
---
+++ src/core/category/tests/application/use_cases/unit/test_update_category.py
@@ -0,0 +1,58 @@
+from unittest.mock import create_autospec
+import uuid
+from core.category.application.category_repository import CategoryRepository
+from core.category.domain.category import Category
+from core.category.application.use_cases.update_category import UpdateCategory, UpdateCategoryRequest
+
+
+class TestUpdateCategory:
+    def test_update_category_name(self):
+        category = Category(
+            id=uuid.uuid4(),
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True
+        )
+
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = category
+
+        use_case = UpdateCategory(repository=mock_repository)
+        request = UpdateCategoryRequest(
+            id=category.id,
+            name="Série"
+        )
+        use_case.execute(request)
+
+        assert category.name == "Série"
+        assert category.description == "Categoria para filmes"
+        mock_repository.update.assert_called_once_with(category)
+
+    def test_update_category_description(self):
+        category = Category(
+            id=uuid.uuid4(),
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True
+        )
+
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = category
+
+        use_case = UpdateCategory(repository=mock_repository)
+        request = UpdateCategoryRequest(
+            id=category.id,
+            description="Categoria para séries"
+        )
+
+        use_case.execute(request)
+
+        assert category.name == "Filme"
+        assert category.description == "Categoria para séries"
+        mock_repository.update.assert_called_once_with(request)
+
+    def test_can_deactivate_category(self):
+        pass
+
+    def test_can_activate_category(self):
+        pass
