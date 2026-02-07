92746e05d668b5cde10bf2f71584a06b96f79aa3
flvSantos15 <flvsantos300@gmail.com>
2026-02-07 13:45:30 -0300

    chore: add delete genre integration test

--- src/core/genre/tests/application/use_cases/integration/test_delete_category.py
+++ src/core/genre/tests/application/use_cases/integration/test_delete_category.py
@@ -1,18 +1,12 @@
 import uuid
-from core.category.application.use_cases.delete_category import DeleteCategory, DeleteCategoryRequest
-from core.category.application.use_cases.get_category import GetCategory
 from core.category.domain.category import Category
-from core.category.infra.in_memory_category import InMemoryCategoryRepository
+from core.genre.application.use_cases.delete_genre import DeleteGenre
+from core.genre.domain.genre import Genre
+from core.genre.infra.in_memory_genre import InMemoryGenreRepository
 
 
 class TestDeleteCategory:
-    def test_delete_category_from_repository(self):
-        category_filme = Category(
-            id=uuid.uuid4(),
-            name="Filme",
-            description="Categoria para filmes",
-            is_active=True
-        )
+    def test_delete_genre_from_repository(self):
         category_serie = Category(
             id=uuid.uuid4(),
             name="Série",
@@ -20,18 +14,20 @@ class TestDeleteCategory:
             is_active=True
         )
 
-        repository = InMemoryCategoryRepository(
-            categories=[
-                category_filme,
-                category_serie
-            ]
+        genre = Genre(
+            id=uuid.uuid4(),
+            name="Drama",
+            is_active=True,
+            categories={category_serie.id}
         )
 
-        use_case = DeleteCategory(repository=repository)
-        request = DeleteCategoryRequest(id=category_filme.id)
+        repository = InMemoryGenreRepository(genres=[genre])
 
-        assert repository.get_by_id(category_filme.id) is not None
+        use_case = DeleteGenre(repository=repository)
+        request = DeleteGenre.Input(id=genre.id)
+
+        assert repository.get_by_id(genre.id) is not None
         response = use_case.execute(request)
 
-        assert repository.get_by_id(category_filme.id) is None
+        assert repository.get_by_id(genre.id) is None
         assert response is None
